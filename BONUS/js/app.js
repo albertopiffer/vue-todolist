5ce8ddede8b683cfe1fdb25d61ade1ba2f33e8bf
albertopiffer <albertopiffer96@gmail.com>
2023-02-21 23:05:14 +0100

    START bonus

---
+++ BONUS/js/app.js
@@ -0,0 +1,52 @@
+const { createApp } = Vue
+
+createApp({
+	data() {
+		
+        return {
+			tasks: [
+				{
+                    text: 'task1',
+                    done: false,
+                },
+
+                {
+                    text: 'task2',
+                    done: true,
+                },
+
+                {
+                    text: 'task3',
+                    done: false,
+                },
+
+                {
+                    text: 'task4',
+                    done: false,
+                },
+			],
+            
+			inputValue: '',
+		}
+
+	},
+
+	methods: {
+		addTask() {
+			const newTask = {
+                text: this.inputValue,
+                done: false,
+            }
+			this.tasks.push(newTask)
+			this.inputValue = ''
+		},
+		removeTask(startIndex) {
+			console.log(
+				'elimino il task con indice:',
+				startIndex,
+				this.tasks[startIndex]
+			)
+			this.tasks.splice(startIndex, 1)
+		},
+	},
+}).mount('#app')
