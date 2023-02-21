4a62d21389a460a709d3ce2c6d72437cd2f679f9
albertopiffer <albertopiffer96@gmail.com>
2023-02-21 17:31:07 +0100

    FEAT array di oggetti 'x' remove

---
+++ js/app.js
@@ -0,0 +1,49 @@
+const { createApp } = Vue
+
+createApp({
+	data() {
+		
+        return {
+			tasks: [
+				{
+                    text: 'task1',
+                    done: false,
+                },
+
+                {
+                    text: 'task2',
+                    done: false,
+                },
+
+                {
+                    text: 'task3',
+                    done: false,
+                },
+
+                {
+                    text: 'task4',
+                    done: false,
+                },
+			],
+			inputValue: '',
+		}
+
+	},
+
+	methods: {
+		addTask() {
+			// console.log('add task:', this.inputValue)
+			const newTask = this.inputValue
+			this.tasks.push(newTask)
+			this.inputValue = ''
+		},
+		removeTask(startIndex) {
+			console.log(
+				'elimino il task con indice:',
+				startIndex,
+				this.tasks[startIndex]
+			)
+			this.tasks.splice(startIndex, 1)
+		},
+	},
+}).mount('#app')
